2dd22c4dea8035ee01cac24099565524d9eb69b2
myjhye <t01087633799@gmail.com>
2023-10-14 00:08:37 +0900

    부서 등록 함수, 부서 등록 핸들러 생성 (재커밋)

--- src/services/DepartmentService.js
+++ src/services/DepartmentService.js
@@ -3,7 +3,12 @@ import axios from "axios";
 const REST_API_BASE_URL = 'http://localhost:8080/api/departments';
 
 
-// 전체 직원 조회
+// 전체 부서 조회
 export const listDepartments = () => {
     return axios.get(REST_API_BASE_URL);
+}
+
+// 부서 등록
+export const createDepartment = (department) => {
+    return axios.post(REST_API_BASE_URL, department);
 }
