e04ca41fc350514076c0a3798df89cc124a77995
myjhye <t01087633799@gmail.com>
2023-10-28 22:46:08 +0900

    로그인 유효성 검사 -> 아이디, 비번 필드 값 없을 때

--- src/pages/LoginComponent.jsx
+++ src/pages/LoginComponent.jsx
@@ -6,7 +6,9 @@ export default function LoginComponent() {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+    const [loginError, setLoginError] = useState("");
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
 
     const navigate = useNavigate();
 
@@ -16,6 +18,11 @@ export default function LoginComponent() {
     async function handleLoginForm(e) {
 
         e.preventDefault();
+
+        // 아이디, 비번 필드 값 변경 시 에러 메시지 초기화
+        setUsernameError("");
+        setPasswordError("");
+        setLoginError("");
 
         const loginObj = { username, password }
 
@@ -48,8 +55,26 @@ export default function LoginComponent() {
             .catch(error => {
 
               if (error.response && error.response.status === 401) {
-                setErrorMessage("계정이나 비밀번호를 확인하세요");
+
+                // 아이디, 비번 모두 입력 되었을 때만 에러 메세지 표시
+                if (username && password) {
+                  setLoginError("계정이나 비밀번호를 확인하세요");
+                }
               }
+
+              if (!username && !password) {
+                setUsernameError("아이디를 입력하세요");
+                return;
+              }
+
+              if (!username) {
+                setUsernameError("아이디를 입력하세요");
+              }
+
+              if (!password) {
+                setPasswordError("비밀번호를 입력하세요");
+              }
+
             })
     }
 
@@ -102,10 +127,20 @@ export default function LoginComponent() {
                   </form>
                 </div>
               </div>
-              {/* 로그인 에러 문구 */}
-              {errorMessage && (
+              {/* 로그인 에러 문구 -> 아이디나 비번 틀림 */}
+              {loginError && (
                 <div className="alert alert-danger">
-                  {errorMessage}
+                  {loginError}
+                </div>
+              )}
+              {usernameError && (
+                <div className="alert alert-danger">
+                  {usernameError}
+                </div>
+              )}
+              {passwordError && (
+                <div className="alert alert-danger">
+                  {passwordError}
                 </div>
               )}
             </div>
